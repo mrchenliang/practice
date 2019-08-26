201de03e16c16089d552311f31186f0b24607519
Chen Liang <c.liang@ualberta.ca>
2019-08-26 01:41:11 +0000

    removing zeroes 283.js

--- LeetCode/283.js
+++ LeetCode/283.js
@@ -1,12 +1,12 @@
 var moveZeroes = function(nums) {
-  for (let i = 0; i < nums.length; i++) {
+  for (let i = nums.length; i--;) {
     if (nums[i] === 0) {
-      let temp = nums.splice(i, 1);
-      nums.push(temp[0]);
+      nums.splice(i, 1);
+      nums.push(0);
     }
   }
   return nums;
 };
 
 console.log(moveZeroes([0,1,0,3,12]))
-console.log(moveZeroes([0,1,0]))
+console.log(moveZeroes([0,0,1]))
