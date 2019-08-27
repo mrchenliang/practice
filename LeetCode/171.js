f9eb7364e6c79d4910c0f1db1803d4489393e264
Chen Liang <c.liang@ualberta.ca>
2019-08-27 05:00:40 +0000

    convert from letters to numbers

--- LeetCode/171.js
+++ LeetCode/171.js
@@ -1,7 +1,11 @@
-var titleToNumber = function(s) {
+const titleToNumber = function(s) {
+  s.toUpperCase();
+  let result = "";
   for (let i = 0; i < s.length; i++) {
-    let a = s.charAt(i) - 64;
+    let a = s.charCodeAt(i) - 64;
     result = result * 26 + a;
   }
   return result;
 };
+
+console.log(titleToNumber("AZ"));
