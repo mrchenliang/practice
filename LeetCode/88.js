95e96f8856afcd90288cbf40a860a0b37dd5093a
Chen Liang <c.liang@ualberta.ca>
2019-08-31 04:13:20 +0000

    updating solution

--- LeetCode/88.js
+++ LeetCode/88.js
@@ -1,18 +1,7 @@
 const merge = function(nums1, m, nums2, n) {
-  let x = 0, y = 0;
-
   nums1.splice(m, nums1.length);
-  nums2.splice(n, nums2.length);
-
-  while (y < n) {
-    if (nums2[y] < nums1[x]) {
-      nums1.splice(x, 0, nums2[y]);
-      x++;
-      y++;
-    } else {
-      x++;
-    }
-  }
+  nums1.push(...nums2);
+  nums1.sort((a,b)=>a-b);
 };
 
 nums1 = [1, 2, 3, 0, 0, 0];
