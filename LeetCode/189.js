53de8be8384b4da1ede3671c20b98676d7aec7e7
Chen Liang <c.liang@ualberta.ca>
2019-08-31 03:34:34 +0000

    updates

--- LeetCode/189.js
+++ LeetCode/189.js
@@ -1,15 +1,15 @@
 const rotate = function(nums, k) {
-  let number = nums.length - k
-  result = [];
-  for (let i = number; i < nums.length; i++) {
-    result.push(nums[i]);
-  }
-  for (let j = 0; j < number; j++) {
-    result.push(nums[j]);
+  let result = [];
+  for (var i = 0; i < nums.length; i++) {
+    if (nums[i + k]) {
+      result[i + k] = nums[i];
+    } else {
+      result[i - (nums.length - k)] = nums[i];
+    }
   }
   return result;
 };
 
 nums = [1, 2, 3, 4, 5, 6, 7];
 k = 3;
-console.log(rotate(nums,k))
+console.log(rotate(nums, k));
