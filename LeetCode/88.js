aab3057eec63ba732e39f691f5d0cf8925e846bf
Chen Liang <c.liang@ualberta.ca>
2019-08-31 04:08:18 +0000

    better solution

--- LeetCode/88.js
+++ LeetCode/88.js
@@ -1,13 +1,23 @@
-var merge = function(nums1, m, nums2, n) {
-  nums1 = nums1.slice(0,(m));
-  nums2 = nums2.slice(0,(n));
-  return nums1.concat(nums2).sort();
+const merge = function(nums1, m, nums2, n) {
+  let x = 0, y = 0;
+
+  nums1.splice(m, nums1.length);
+  nums2.splice(n, nums2.length);
+
+  while (y < n) {
+    if (nums2[y] < nums1[x]) {
+      nums1.splice(x, 0, nums2[y]);
+      x++;
+      y++;
+    } else {
+      x++;
+    }
+  }
 };
 
-
-nums1 = [1,2,3,0,0,0];
+nums1 = [1, 2, 3, 0, 0, 0];
 m = 3;
-nums2 = [2,5,6];
+nums2 = [2, 5, 6];
 n = 3;
 
-merge(nums1, m, nums2, n);
+console.log(merge(nums1, m, nums2, n));
